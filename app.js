1e94e4ac821042ca83b34f5e7cf9d79e37bede05
MauricioJrB <mjunior1205@gmail.com>
2023-11-23 14:33:53 -0300

    Alteração ne arquivo banco.js. Devido a um bug foi necessário criar um middleware de acesso para exportar a conexão.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const conectarBanco = require('./helpers/banco');
+
 var app = express();
 
 app.use(logger('dev'));
@@ -17,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(conectarBanco);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- helpers/banco.js
+++ helpers/banco.js
@@ -6,15 +6,16 @@ dotenv.config();
 const username = process.env.MONGO_USERNAME;
 const password = process.env.MONGO_PASSWORD;
 
+const conectarBanco = (req, res, next) => {
+    mongoose.connect(
+        `mongodb+srv://${username}:${password}@api-rest.yn6gipj.mongodb.net/?retryWrites=true&w=majority`,
+    ).then(() => {
+        console.log('Conectado ao banco de dados');
+    }).catch((err) => {
+        console.log('Erro ao conectar ao banco de dados');
+        console.log(err);
+    });
+}
 
-mooongose.connect(
-    `mongodb+srv://${username}:${password}@api-rest.yn6gipj.mongodb.net/?retryWrites=true&w=majority`,
-).then(() => {
-    console.log('Conectado ao banco de dados');
-}).catch((err) => {
-    console.log('Erro ao conectar ao banco de dados');
-    console.log(err);
-});
-
-module.exports = mongoose;
+module.exports = conectarBanco;
 

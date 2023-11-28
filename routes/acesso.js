7e089cb6c8029287004ebaf42e68de53891e3505
Victor Hugo Favaro Moreira <zvihugo@hotmail.com>
2023-11-27 22:15:25 -0300

    Adicionei mais duas rotas, que é a de PUT e GET, uma para atualizar e outra para buscar um ID

--- routes/acesso.js
+++ routes/acesso.js
@@ -91,31 +91,62 @@ router.delete("/excluir/:nome", validaNome, usuarioOuAdmin, async(req, res) => {
 
 //Rota alterar dados pessoas
 router.put('/users/:id', usuarioOuAdmin, async (req, res) => {
+    const id = req.params.id;
+    const updates = req.body;
+
     try {
-      const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  
-      if (!user) {
-        return res.status(404).send();
-      }
-  
-      res.send(user);
+        let user;
+        if(id.endsWith(ADMIN)){
+            user = await admins.buscarPorId(id);
+        }else{
+            user = await usuarios.buscarPorId(id);
+        }
+
+        if (!user) {
+            return res.status(404).json({ msg: "Usuário não encontrado" });
+        }
+
+        if(updates.nome && updates.nome.endsWith(ADMIN)){
+            return res.status(400).json({ msg: "Não é permitido alterar um usuário para administrador" });
+        }
+
+        if(updates.nome){
+            user.nome = updates.nome;
+        }
+        if(updates.senha){
+            user.senha = updates.senha;
+        }
+
+        if(id.endsWith(ADMIN)){
+            await admins.atualizar(id, user);
+        }else{
+            await usuarios.atualizar(id, user);
+        }
+
+        res.status(200).json({ msg: "Usuário atualizado com sucesso", user: user });
     } catch (e) {
-      res.status(400).send(e);
+        res.status(500).json({ msg: "Aconteceu um erro no servidor." });
     }
-  });
+});
 
-
-//Rota para buscar um ID
-/*router.get("/buscar/:id", validaId, async(req, res) => {
+// Rota para buscar um ID
+router.get("/buscar/:id", validaId, async(req, res) => {
     const id = req.params.id;
 
     try {
-        const user = await usuarios.buscar(id);
+        let user = await usuarios.buscarPorId(id);
+        if (!user) {
+            user = await admins.buscarPorId(id);
+        }
+
+        if (!user) {
+            return res.status(404).json({ msg: "Usuário não encontrado" });
+        }
+
         res.status(200).json(user);
     } catch (e) {
-        res.status(400).json({ msg: "Usuário não encontrado" });
+        res.status(500).json({ msg: "Erro ao buscar usuário" });
     }
 });
-*/
 
 module.exports = router;
